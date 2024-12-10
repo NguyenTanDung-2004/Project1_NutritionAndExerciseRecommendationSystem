ca815025f3eef6c106c4ad52784ac3e45f7e82ff
GiangPhanNguyenTra <22520360@gm.uit.edu.vn>
2024-12-11 01:40:22 +0700

    feat: add detail of goal page

---
+++ frontend-project1/src/component/goals/details/GoalDay.js
@@ -0,0 +1,52 @@
+import React from "react";
+import GoalChart from "../../chart/GoalChart";
+
+const GoalDay = ({ date }) => {
+  const data = {
+    requiredProtein: 100,
+    requiredFat: 50,
+    requiredCarb: 200,
+    consumedProtein: 90,
+    consumedFat: 45,
+    consumedCarb: 180,
+    burned: 500,
+  };
+
+  const comment = {
+    calo: 50,
+    kg: "+0.5",
+  };
+
+  // Kiểm tra nếu comment có dữ liệu hay không
+  const hasComment = comment && (comment.calo || comment.kg);
+
+  return (
+    <div className="mt-10 w-full bg-white p-6 rounded-2xl shadow-md mb-10">
+      <h1 className="text-base font-bold mb-4 text-[#202224]">{date}</h1>
+
+      <div className="w-full flex flex-wrap gap-10 justify-between items-center sm:px-4 md:px-8 lg:px-10">
+        <GoalChart data={data} />
+        <div className="flex flex-1 flex-col gap-4 sm:mb-0 md:mb-0 lg:mb-10">
+          <div className="text-[#202224] font-bold">• Nhận xét</div>
+
+          {hasComment ? (
+            <>
+              <div className="text-[#828181] mt-2 ml-4">
+                - Độ chênh lệch calories: {comment.calo} calo
+              </div>
+              <div className="text-[#828181] mt-2 ml-4">
+                - Số kg thay đổi: {comment.kg}kg
+              </div>
+            </>
+          ) : (
+            <div className="text-[#828181] mt-2 ml-4 italic">
+              chưa có nhận xét nào
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GoalDay;
--- frontend-project1/src/component/goals/details/GoalOverview.js
+++ frontend-project1/src/component/goals/details/GoalOverview.js
@@ -1,7 +1,124 @@
 import React from "react";
+import LineProgress from "../LineProgress";
 
-const GoalOverview = () => {
-  return <div></div>;
+const GoalOverview = ({ id }) => {
+  // Dữ liệu mẫu
+  const data = {
+    target: "-2",
+    name: "Mục tiêu giảm cân",
+    percentage: 75,
+    daysCompleted: 6,
+    totalDays: 7,
+    startDate: "01/12/2024",
+    endDate: "07/12/2024",
+    type: "Giảm cân",
+    status: "Đang thực hiện",
+  };
+
+  const comment = {
+    calo: "+200",
+    kgChange: "+0.5",
+    kgNeed: "-0.5",
+    time: "2 ngày",
+  };
+
+  const hasComment =
+    comment &&
+    (comment.calo || comment.kgChange || comment.kgNeed || comment.time);
+
+  // Xử lý màu sắc dựa trên loại và trạng thái
+  const typeColor =
+    data.type === "Giảm cân"
+      ? "bg-[#00B69B] text-[#00B69B]"
+      : "bg-[#FF9500] text-[#FF9500]";
+  const statusColor =
+    data.status === "Đang thực hiện"
+      ? "bg-[#1A78F2] text-[#1A78F2]"
+      : "bg-[#595858] text-[#595858]";
+
+  return (
+    <div className="mt-10 w-full bg-white p-6 rounded-2xl shadow-md flex flex-col gap-4">
+      <div className="w-full flex gap-4 items-center rounded-2xl ">
+        <div className="flex g-4">
+          <div className="flex bg-[#D9D9D9] rounded-md p-4 justify-center items-center text-xl">
+            {data.target}kg
+          </div>
+        </div>
+
+        <div className="flex-1 flex flex-col gap-2 w-auto">
+          <div className="text-black rounded-md text-base">{data.name}</div>
+          <div className="flex items-center gap-3">
+            <LineProgress percentage={data.percentage} />
+            <div className="text-[#787878] rounded-md text-sm">{`${data.daysCompleted}/${data.totalDays} ngày`}</div>
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-2 self-end">
+          <div className="flex gap-2">
+            <div
+              className={`flex relative gap-4 justify-between items-start px-4 py-1.5 min-h-[27px] ${typeColor} bg-opacity-20 rounded-md`}
+            >
+              <div className="z-0 flex-1 shrink my-auto basis-0 font-Averta-Bold text-[13px]">
+                {data.type}
+              </div>
+            </div>
+            <div
+              className={`flex relative gap-4 justify-between items-start px-4 py-1.5 min-h-[27px] ${statusColor} bg-opacity-20 rounded-md`}
+            >
+              <div className="z-0 flex-1 shrink my-auto basis-0 font-Averta-Bold text-[13px]">
+                {data.status}
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col gap-1 ml-auto">
+            <div className="flex justify-between">
+              <div className="text-xs text-[#595858] font-semibold mr-1">
+                Ngày bắt đầu
+              </div>
+              <div className="text-xs text-[#888888] text-right">
+                {data.startDate}
+              </div>
+            </div>
+            <div className="flex justify-between">
+              <div className="text-xs text-[#595858] font-semibold mr-1">
+                Ngày kết thúc
+              </div>
+              <div className="text-xs text-[#888888] text-right">
+                {data.endDate}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="ml-20 flex flex-1 flex-col mt-10 gap-4 sm:mb-0 md:mb-0 lg:mb-10">
+        <div className="text-[#202224] font-bold text-center sm:text-left">
+          • Nhận xét chung
+        </div>
+
+        {hasComment ? (
+          <>
+            <div className="text-[#828181] mt-2 ml-4 sm:ml-0">
+              - Lượng calories đã tiêu thụ: {comment.calo} calo
+            </div>
+            <div className="text-[#828181] ml-4 sm:ml-0">
+              - Số kg thay đổi: {comment.kgChange}kg
+            </div>
+            <div className="text-[#828181] ml-4 sm:ml-0">
+              - Số kg cần thay đổi để đạt mục tiêu: {comment.kgNeed}kg
+            </div>
+            <div className="text-[#828181] ml-4 sm:ml-0">
+              - Thời gian còn lại: {comment.time}
+            </div>
+          </>
+        ) : (
+          <div className="text-[#828181] mt-2 ml-4 sm:ml-0 italic">
+            chưa có nhận xét nào
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default GoalOverview;
